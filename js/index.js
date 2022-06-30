f2d223eee6d2919b56946da4426944be9636c6c4
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-06-30 19:22:51 -0400

    Add slider indicator and background images

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const testimonials = document.querySelectorAll(".testimonial");
+const indicators = document.querySelectorAll(".indicator");
 
 let index = 0;
 setInterval(() => {
   testimonials.forEach((testimonial) => {
     testimonial.classList.remove("active");
   });
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("active");
+  });
   testimonials[index].classList.add("active");
+  indicators[index].classList.add("active");
   index = index === testimonials.length - 1 ? 0 : index + 1;
 }, 3000);
